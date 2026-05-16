f9d7dcca18c6898cfd1a40c091083eef5a9cf96e
doctotypetech-dotcom <doctotype.tech@gmail.com>
2026-05-16 15:04:42 +0200

    Refactor install.sh for better readability and structure
    Updated installation script for hostapd with improved echo statements and formatting.

--- install.sh
+++ install.sh
@@ -1,19 +1,25 @@
-echo "Instalation des dépenadances"
+#!/bin/bash
+
+echo "(1/7) Instalation des dépenadances"
 sudo apt-get install hostapd dnsmasq iptables-persistent -y
 cd /tmp
-echo "Téléchargement de la bibliothèque hostapd"
+
+echo "(2/7) Téléchargement de la bibliothèque hostapd"
 git clone https://git.w1.fi/hostap.git
 mkdir temp
 cd temp
 git clone https://github.com/doctotypetech-dotcom/hostapd.git
 cd hostapd
-echo "Application des Fix"
-cp fix.h /tmp/hostapd/src/ap/sta_info.h
+
+echo "(3/7) Application des Fix"
+cp fix.h /tmp/hostap/src/ap/sta_info.h
 cd /tmp/hostap/hostapd
-echo "Compilation"
+
+echo "(4/7)Compilation"
 cp defconfig .config
-make
-echo "Copie des nouveau fichiers"
+make -j$(nproc)
+
+echo "(5/7)Copie des nouveau fichiers"
 mv /usr/sbin/hostapd /usr/sbin/hostapd.bak
 sudo cp hostapd /usr/sbin/hostapd
 cd /tmp/temp/hostapd/
@@ -21,6 +27,8 @@ sudo rm /etc/dnsmasq.conf
 sudo rm /etc/hostapd/hostapd.conf
 sudo cp dnsmasq.conf /etc/dnsmasq.conf
 sudo cp hostapd.conf /etc/hostapd/hostapd.conf
+
+echo "(6/7) Application des règles réseau"
 echo "net.ipv4.ip_forward=1" | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
@@ -29,6 +37,8 @@ sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
 sudo netfilter-persistent save
 sudo systemctl enable dnsmasq
 sudo systemctl enable hostapd
+
+echo "(7/7) Terminé"
 echo "Pour changer le nom du Wi-Fi et son mot de passe, entrez : "
 echo "sudo nano /etc/hostapd/hostapd.conf"
 echo "Pour appliquer les changements, veilliez redémarrer."
